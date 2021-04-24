ec7ddcd99c2ae6b821db485f34ebdc397caa4d35
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2021-04-24 19:38:52 +0300

    commit

--- myscript.js
+++ myscript.js
@@ -10,10 +10,12 @@ $(document).ready(function(){
       var td1 = document.createElement('td');
     
       td1.bgColor = getRandomColor();
-      td1.style.width = "20px";
-      td1.style.height = "20px";
+      td1.style.width = "30px";
+      td1.style.height = "30px";
       
-      td1.onclick = function(){ tdclickFunc($(this)); };
+      td1.onclick = function(){ tdclickFunc(this); };
+      td1.onmouseover = function(){ tdOnMouseOver(this); };
+      td1.onmouseleave = function(){ tdOnMouseLeave(this); };
     
       tr.appendChild(td1);
         
@@ -24,8 +26,15 @@ $(document).ready(function(){
   document.body.appendChild(table);
 
   function tdclickFunc(x){
-    x.bgColor = "#FFF";
     alert("Merhaba");
+  }
+
+  function tdOnMouseOver(x){
+    x.style.opacity = "0.5";
+  }
+
+  function tdOnMouseLeave(x){
+    x.style.opacity = "1";
   }
 
   function getRandomColor() {
