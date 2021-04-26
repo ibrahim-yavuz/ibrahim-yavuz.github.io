f191319508367454698b53125db173d2694f427b
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2021-04-26 18:29:09 +0300

    commit

--- myscript.js
+++ myscript.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
   });
   
   $(".yuksek_skor").text("Yüksek Skor: " + localStorage.getItem("high_score"));
+  $(".skor").text("Yüksek Skor: " + localStorage.getItem("score"));
   $(".basla").click(function(){
     basla();
     createTable();
